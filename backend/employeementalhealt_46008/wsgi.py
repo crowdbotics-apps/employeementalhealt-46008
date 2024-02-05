804863cd487dc99ab693f0415dd5ea27b90ad290
Crowdbotics <team@crowdbotics.com>
2024-02-05 22:13:45 +0000

    Initial commit

---
+++ backend/employeementalhealt_46008/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for employeementalhealt_46008 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "employeementalhealt_46008.settings"
+)
+
+application = get_wsgi_application()
